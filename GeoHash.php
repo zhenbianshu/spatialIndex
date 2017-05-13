46902d0c61895a784189d6aa68a92ee7e8a5b10f
pengfei29 <pengfei29@bj-m-206726a.local>
2017-05-13 14:58:05 +0800

    完善geohash方法，添加测试方法

--- GeoHash.php
+++ GeoHash.php
@@ -2,48 +2,120 @@
 
 class GeoHash
 {
-    const DOUBLE_LENGTH = 64;
+    const LEVEL_MAX = 26; // 26位已能达到 0.6m 的精度
+    const MERCATOR_LENGTH = 40075452.74; // 墨卡托投影下地球的赤道周长
     private static $LNG_RANGE = ['min' => -180, 'max' => 180];
     private static $LAT_RANGE = ['min' => -90, 'max' => 90];
 
-    private $round_bin;
-    private $bits_bin;
-    private $lng;
-    private $lat;
-    private $bits_lng;
-    private $bits_lat;
-    private $level = 26;// 26位已能达到 0.6m 的精度
-
-    public function __construct($lng, $lat, $level)
+    public function geoRadius()
     {
-        // todo :validate lng lat precision
-
-        $this->lng = $lng;
-        $this->lat = $lat;
-        $this->level = $level ?: $this->level;
+        /**
+         *  数据准备：将地理点通过 getHashInt($lng, $lat) 获取其 hashInt 值，存入redis 的 sorted set
+         *  查询：
+         *      1. 通过 getIntLimit($center_lng, $center_lat, $radius); 获取到九个方格的 hashInt 值 和 其 range
+         *      2. 用 ZRANGEBYSCORE key hashInt hashInt+range 获取九个方格内的地址点
+         *      3. 遍历各个点（九个方格点范围略大），计算距离，返回严格符合要求的点
+         */
     }
 
-    public function getHashBin()
+    /**
+     * 获取某点geohash对应的Int值
+     *
+     * @param $lng
+     * @param $lat
+     *
+     * @return number
+     */
+    public function getHashInt($lng, $lat)
     {
-        $this->bits_lng = $this->getBits($this->lng, self::$LNG_RANGE);
-        $this->bits_lat = $this->getBits($this->lat, self::$LAT_RANGE);
+        $bits_lng = $this->getBits($lng, self::$LNG_RANGE);
+        $bits_lat = $this->getBits($lat, self::$LAT_RANGE);
 
+        $bits = $this->assembleBits($bits_lng, $bits_lat);
 
-        $this->round_bin = $this->getAssembled($this->bits_lng, $this->bits_lat);
-        return $this->round_bin;
+        return $this->getFormalInt($bits);
     }
 
-    public function getHashInt()
+    /**
+     * 测试某一目的点是否在中心点N米范围内
+     *
+     * @param $center_lng
+     * @param $center_lat
+     * @param $radius
+     * @param $aim_lng
+     * @param $aim_lat
+     */
+    public function testInRadius($center_lng, $center_lat, $radius, $aim_lng, $aim_lat)
     {
-        $bits_bin = $this->getHashBin();
-        $bits_padded = str_pad($bits_bin, $this->level * 2, '0');
+        $limit = $this->getIntLimit($center_lng, $center_lat, $radius);
+        $int_aim = $this->getHashInt($aim_lng, $aim_lat);
+
+        $res = 'not_in_radius';
+        foreach ($limit['cells'] as $int_cell) {
+            if ($int_aim > $int_cell && $int_aim < $int_cell + $limit['range']) {
+                $res = 'in_radius';
+                break;
+            }
+        }
+        echo $res . PHP_EOL;
+    }
+
+    /**
+     * 通过二进制哈希串获取到规范化为52位的正整数
+     *
+     * @param $bits
+     *
+     * @return number
+     */
+    private function getFormalInt($bits)
+    {
+        $bits_padded = str_pad($bits, self::LEVEL_MAX * 2, '0');
+
         return bindec($bits_padded);
     }
 
-    private function getBits($loc, $range)
+    /**
+     * 获取int值的限制
+     *
+     * @param $lng
+     * @param $lat
+     * @param $radius
+     *
+     * @return array
+     */
+    private function getIntLimit($lng, $lat, $radius)
+    {
+        $level = $this->getLevel($radius);
+        $bits_lng = $this->getBits($lng, self::$LNG_RANGE, $level);
+        $bits_lat = $this->getBits($lat, self::$LAT_RANGE, $level);
+
+        $cells = $this->getRoundCells($bits_lng, $bits_lat);
+        $cells['mid'] = $this->getFormalInt($this->assembleBits($bits_lng, $bits_lat));
+
+        $range = $this->getLevelRange($level);
+
+        $limit = [
+            'cells' => $cells,
+            'range' => $range,
+        ];
+
+        return $limit;
+    }
+
+
+    /**
+     * 通过经度/纬度 和其范围值获取到其二进制哈希串
+     *
+     * @param $loc
+     * @param $range
+     * @param int $level
+     *
+     * @return string
+     */
+    private function getBits($loc, $range, $level = self::LEVEL_MAX)
     {
         $bits = '';
-        for ($i = 0; $i < $this->level; $i++) {
+        for ($i = 0; $i < $level; $i++) {
             $mid = ($range['min'] + $range['max']) / 2;
             if ($loc < $mid) {
                 $bits .= '0';
@@ -54,15 +126,22 @@ class GeoHash
             }
         }
 
-        $this->bits_bin = $bits;
         return $bits;
     }
 
-    private function getAssembled($lng, $lat)
+    /**
+     * 组合经度和纬度的二进制串
+     *
+     * @param $lng
+     * @param $lat
+     *
+     * @return string
+     */
+    private function assembleBits($lng, $lat)
     {
         $bits_assembled = '';
-        $arr_lng = explode('', $lng);
-        $arr_lat = explode('', $lat);
+        $arr_lng = str_split($lng);
+        $arr_lat = str_split($lat);
 
         for ($i = 0, $c = count($arr_lng); $i < $c; $i++) {
             $bits_assembled .= $arr_lat[$i] . $arr_lng[$i];
@@ -71,64 +150,73 @@ class GeoHash
         return $bits_assembled;
     }
 
-    private function getRoundBin($about = 1)
+    /**
+     * 获取中心格子四周的八个格子
+     *
+     * @param $bits_lng
+     * @param $bits_lat
+     *
+     * @return mixed
+     */
+    private function getRoundCells($bits_lng, $bits_lat)
     {
-        $accuracy = pow(2, $about - 1);
-        $lng_incr = decbin(bindec($this->bits_lng) + $accuracy);
-        $lng_decr = decbin(bindec($this->bits_lng) - $accuracy);
+        $lng_incr = decbin(bindec($bits_lng) + 1);
+        $lng_decr = decbin(bindec($bits_lng) - 1);
 
-        $lat_incr = decbin(bindec($this->bits_lat) + $accuracy);
-        $lat_decr = decbin(bindec($this->bits_lat) - $accuracy);
+        $lat_incr = decbin(bindec($bits_lat) + 1);
+        $lat_decr = decbin(bindec($bits_lat) - 1);
 
-        $round['up'] = $this->getAssembled($this->bits_lng, $lat_incr);
-        $round['down'] = $this->getAssembled($this->bits_lng, $lat_decr);
-        $round['left'] = $this->getAssembled($lng_decr, $this->bits_lat);
-        $round['right'] = $this->getAssembled($lng_incr, $this->bits_lat);
-        $round['left_up'] = $this->getAssembled($lng_decr, $lat_incr);
-        $round['right_up'] = $this->getAssembled($lng_incr, $lat_incr);
-        $round['left_down'] = $this->getAssembled($lng_incr, $lat_decr);
-        $round['right_down'] = $this->getAssembled($lng_incr, $lat_decr);
+        $cells['mid'] = $this->getFormalInt($this->assembleBits($bits_lng, $bits_lat));
+        $cells['up'] = $this->getFormalInt($this->assembleBits($bits_lng, $lat_incr));
+        $cells['down'] = $this->getFormalInt($this->assembleBits($bits_lng, $lat_decr));
+        $cells['left'] = $this->getFormalInt($this->assembleBits($lng_decr, $bits_lat));
+        $cells['right'] = $this->getFormalInt($this->assembleBits($lng_incr, $bits_lat));
+        $cells['left_up'] = $this->getFormalInt($this->assembleBits($lng_decr, $lat_incr));
+        $cells['right_up'] = $this->getFormalInt($this->assembleBits($lng_incr, $lat_incr));
+        $cells['left_down'] = $this->getFormalInt($this->assembleBits($lng_incr, $lat_decr));
+        $cells['right_down'] = $this->getFormalInt($this->assembleBits($lng_incr, $lat_decr));
 
-        return $round;
+        return $cells;
     }
 
-    private function getRoundInt($about = 1)
+    /**
+     * 通过范围值获取geohash层级
+     *
+     * @param $range_meter
+     *
+     * @return int
+     */
+    private function getLevel($range_meter)
     {
+        $level = 0;
+        $global = self::MERCATOR_LENGTH;
+        while ($global > $range_meter) {
+            $global /= 2;
+            $level++;
+        }
 
+        return $level;
+    }
 
+    /**
+     * 通过哈希层级获取每一格子的范围
+     *
+     * @param $level
+     *
+     * @return number
+     */
+    private function getLevelRange($level)
+    {
+        $range = pow(2, 2 * (self::LEVEL_MAX - $level));
+
+        return $range;
     }
 }
 
-
-
-
-/*
- *
- *  通过墨卡托投影下最大半径 获取到此半径应该使用的level
- *  最后将level和周边内的格子内的数据计算一遍，得到每个格子的最大、最小的int值，将数据取出来后过滤掉距离不对的。
- *
- *
- *
- uint8_t geohashEstimateStepsByRadius(double range_meters, double lat) {
-    if (range_meters == 0) return 26;
-    int step = 1;
-                          // 地球半径
-    while (range_meters < MERCATOR_MAX) {
-        range_meters *= 2;
-        step++;
-    }
-    step -= 2; /* Make sure range is included in most of the base cases. */
-
-    /* Wider range torwards the poles... Note: it is possible to do better
-     * than this approximation by computing the distance between meridians
-     * at this latitude, but this does the trick for now.
-    if (lat > 66 || lat < -66) {
-        step--;
-        if (lat > 80 || lat < -80) step--;
-    }
-
-    /* Frame to valid range.
-    if (step < 1) step = 1;
-    if (step > 26) step = 26;
-    return step;
-}*/
+$geohash = new GeoHash();
+// 中心地址：新浪总部大厦
+$geohash->testInRadius(116.276231, 40.041143, 3000, 116.276301, 40.041532); // 新浪餐厅 in
+$geohash->testInRadius(116.276317, 40.04168, 3000, 116.27236, 40.04214); // 百度科技园 in
+$geohash->testInRadius(116.276317, 40.04168, 3000, 116.274826521, 40.0321647826); // 兰园小区 in
+$geohash->testInRadius(116.276317, 40.04168, 3000, 116.298505, 40.023749); // 上地医院 in
+$geohash->testInRadius(116.276317, 40.04168, 3000, 116.31763, 40.01522); // 圆明园 not in
